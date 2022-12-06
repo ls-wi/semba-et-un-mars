40c00576bf88bcd898889953ff38687f10ab1e31
Sakada Ly <sakada.ly@papernest.com>
2022-12-06 15:14:44 +0100

    initial commit

---
+++ components/Banner.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="banner">
+        <TypeEffect class="bannerText" :words="words"/>
+    </div>
+</template>
+
+<script>
+
+import TypeEffect from './TypingEffect.vue'
+export default{
+    name:'Banner',
+    data() {
+      return {
+        words:['Web Development', 'Digital Marketing', 'Consulting', "Let's sail together!"]
+      }
+    },
+    components:{
+        TypeEffect
+    }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Header/>
+    <Banner/>
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+import Banner from '../components/Banner.vue'
+export default {
+  name: 'IndexPage',
+  components:{
+    Header,
+    Banner
+  }
+}
+</script>
